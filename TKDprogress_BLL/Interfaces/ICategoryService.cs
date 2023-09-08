349f8d91ee34d6ee673ef11369164bfe4cb53f5a
Mylo Rongen <PS220125@summacollege.nl>
2023-09-08 11:07:22 +0200

    Develop test

--- TKDprogress_BLL/Interfaces/ICategoryService.cs
+++ TKDprogress_BLL/Interfaces/ICategoryService.cs
@@ -10,13 +10,9 @@ namespace TKDprogress_BLL.Interfaces
     public interface ICategoryService
     {
         Task<List<CategoryDto>> GetCategoriesAsync(string searchString);
-
         Task<CategoryDto> GetCategoryByIdAsync(int id);
-
         Task<CategoryDto> CreateCategoryAsync(CategoryDto category);
-
         Task<CategoryDto> UpdateCategoryAsync(CategoryDto newCategory);
-
         Task<CategoryDto> DeleteCategoryAsync(CategoryDto category);
     }
 }
