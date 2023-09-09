112acdec3b933d101daec53b8e76ff3d361f3dbb
Mylo Rongen <PS220125@summacollege.nl>
2023-09-09 14:33:00 +0200

    Fixed category page for users and added terminology page which needs some changes later on

--- TKDprogress/Models/CategoryViewModel.cs
+++ TKDprogress/Models/CategoryViewModel.cs
@@ -1,10 +1,14 @@
-﻿namespace TKDprogress.Models
+﻿using TKDprogress_SL.Enums;
+
+namespace TKDprogress.Models
 {
     public class CategoryViewModel
     {
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+
+        public EnumStatus? Status { get; set; }
 
         public List<TerminologyViewModel> Terminologies { get; set; }
     }
